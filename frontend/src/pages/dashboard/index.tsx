638b838847c46e7a27d9adca02e33f37ddccdad9
AlvarengaEric17echo <ericolivealvarenga@gmail.com>
2024-08-09 13:16:16 -0300

    botão logout

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,3 +1,5 @@
+import { useContext } from "react"
+
 import { useState } from 'react'
 
 import { Button } from "../../components/ui/Button"
@@ -14,7 +16,13 @@ import { setupAPIClient } from '../../services/api'
 
 import { AuthProvider } from "../../contexts/AuthContext"
 
+import { AuthContext } from "../../contexts/AuthContext"
+
+import { FiLogOut } from "react-icons/fi"
+
 export default function Dashboard() {
+
+    const { signOut, user } = useContext(AuthContext)
     return (
         <div>
             <Head>
@@ -24,8 +32,12 @@ export default function Dashboard() {
             </Head>
 
             <div className={styles.container}>
-                <h1>Seja bem vindo(a)</h1>
+                <h1>Olá, {user?.name}. Seja bem vindo(a)</h1>
                 <h2>Login efetuado com sucesso</h2>
+
+                <button onClick={signOut}>
+                    <FiLogOut color="black" size={24} />
+                </button>
 
             </div>
         </div>
